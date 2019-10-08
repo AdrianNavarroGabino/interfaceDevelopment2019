d3795fbeb9b467bdd639c3f383c297659458d63c
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-10-08 17:44:39 +0200

    Exercise 01 - MessageBox

---
+++ 03-userInterfacesConfection/02-Ejercicio1/Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 191);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Enter += new System.EventHandler(this.HaveEntered);
+            this.textBox1.Leave += new System.EventHandler(this.IsLeaving);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.IsValidating);
+            this.textBox1.Validated += new System.EventHandler(this.IsValidated);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(504, 191);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Enter += new System.EventHandler(this.HaveEntered);
+            this.textBox2.Leave += new System.EventHandler(this.IsLeaving);
+            this.textBox2.Validating += new System.ComponentModel.CancelEventHandler(this.IsValidating);
+            this.textBox2.Validated += new System.EventHandler(this.IsValidated);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.IsClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.IsClosed);
+            this.Load += new System.EventHandler(this.IsLoaded);
+            this.Shown += new System.EventHandler(this.IsShown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ 03-userInterfacesConfection/02-Ejercicio1/Ejercicio1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        private string message;
+        private string caption;
+        private MessageBoxButtons buttons;
+        private DialogResult result;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void IsLoaded(object sender, EventArgs e)
+        {
+            message = "The form is loaded";
+            caption = "Load";
+            buttons = MessageBoxButtons.OK;
+
+            // Displays the MessageBox.
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                // Closes the parent form.
+                this.Close();
+            }
+        }
+
+        private void IsShown(object sender, EventArgs e)
+        {
+            message = "The form is shown";
+            caption = "Shown";
+            buttons = MessageBoxButtons.OK;
+
+            // Displays the MessageBox.
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                // Closes the parent form.
+                this.Close();
+            }
+        }
+
+        private void IsClosing(object sender, FormClosingEventArgs e)
+        {
+            message = "The form is closing";
+            caption = "Closing";
+            buttons = MessageBoxButtons.OK;
+
+            // Displays the MessageBox.
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                // Closes the parent form.
+                this.Close();
+            }
+        }
+
+        private void IsClosed(object sender, FormClosedEventArgs e)
+        {
+            message = "The form is closed";
+            caption = "Closed";
+            buttons = MessageBoxButtons.OK;
+
+            // Displays the MessageBox.
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                // Closes the parent form.
+                this.Close();
+            }
+        }
+
+        private void HaveEntered(object sender, EventArgs e)
+        {
+            message = "You have entered";
+            caption = "Enter";
+            buttons = MessageBoxButtons.OK;
+
+            // Displays the MessageBox.
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                // Closes the parent form.
+                this.Close();
+            }
+        }
+
+        private void IsValidating(object sender, CancelEventArgs e)
+        {
+            message = "It's validating";
+            caption = "Validating";
+            buttons = MessageBoxButtons.OK;
+
+            // Displays the MessageBox.
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                // Closes the parent form.
+                this.Close();
+            }
+        }
+
+        private void IsValidated(object sender, EventArgs e)
+        {
+            message = "It's validated";
+            caption = "Validated";
+            buttons = MessageBoxButtons.OK;
+
+            // Displays the MessageBox.
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                // Closes the parent form.
+                this.Close();
+            }
+        }
+
+        private void IsLeaving(object sender, EventArgs e)
+        {
+            message = "It's leaving";
+            caption = "Leave";
+            buttons = MessageBoxButtons.OK;
+
+            // Displays the MessageBox.
+            result = MessageBox.Show(message, caption, buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                // Closes the parent form.
+                this.Close();
+            }
+        }
+    }
+}
