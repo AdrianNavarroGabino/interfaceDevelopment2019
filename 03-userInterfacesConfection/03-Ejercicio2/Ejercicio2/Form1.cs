b3c5c49430d9178e3e2a5347e3b4d0b978ed11f4
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-10-08 17:53:33 +0200

    Exercise 02 - Changing forms

---
+++ 03-userInterfacesConfection/03-Ejercicio2/Ejercicio2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    public partial class Form1 : Form
+    {
+        private Form2 form2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            form2 = new Form2(this);
+        }
+
+        private void GoForm2(object sender, EventArgs e)
+        {
+            this.Hide();
+            form2.Show();
+        }
+    }
+}
---
+++ 03-userInterfacesConfection/03-Ejercicio2/Ejercicio2/Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio2
+{
+    public partial class Form2 : Form
+    {
+        private Form1 form1;
+
+        public Form2(Form1 form1)
+        {
+            InitializeComponent();
+            this.form1 = form1;
+        }
+
+        private void GoForm1(object sender, EventArgs e)
+        {
+            this.Hide();
+            form1.Show();
+        }
+
+        private void GoForm1(object sender, FormClosedEventArgs e)
+        {
+            form1.Close();
+        }
+    }
+}
