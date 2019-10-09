07ef5a55d9fc109af9795668fbfb89894443fe77
Adrian Navarro Gabino <adnaga94@gmail.com>
2019-10-09 11:29:35 +0200

    Exercise 05 - ListBox

---
+++ 03-userInterfacesConfection/06-Ejercicio5/Ejercicio5/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Ejercicio5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.show = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(512, 436);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.SelectItem);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 463);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Add);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(79, 514);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 2;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.Add);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(224, 514);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(75, 23);
+            this.delete.TabIndex = 3;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.Delete);
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(379, 514);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 23);
+            this.show.TabIndex = 4;
+            this.show.Text = "Show";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.ShowItem);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 564);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button show;
+    }
+}
+
---
+++ 03-userInterfacesConfection/06-Ejercicio5/Ejercicio5/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio5
+{
+    public partial class Form1 : Form
+    {
+        private Form2 form2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Add(object sender, EventArgs e)
+        {
+            Add();
+        }
+
+        private void SelectItem(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                textBox1.Text =
+                    listBox1.Items[listBox1.SelectedIndex].ToString();
+            }
+            else
+            {
+                textBox1.Text = "";
+            }
+        }
+
+        private void Delete(object sender, EventArgs e)
+        {
+            if(listBox1.Items.Contains(textBox1.Text))
+            {
+                listBox1.Items.Remove(textBox1.Text);
+            }
+        }
+
+        private void Add(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                Add();
+            }
+        }
+
+        private void Add()
+        {
+            if (textBox1.Text != "" && !listBox1.Items.Contains(textBox1.Text))
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";
+            }
+        }
+
+        private void ShowItem(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Contains(textBox1.Text))
+            {
+                form2 = new Form2(this, textBox1.Text);
+                this.Hide();
+                form2.Show();
+            }
+        }
+    }
+}
