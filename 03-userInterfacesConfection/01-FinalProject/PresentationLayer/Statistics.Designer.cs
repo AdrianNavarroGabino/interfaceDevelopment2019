b56237b97fca10c3d4802ba8e2d9f83fc7d1a9d0
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2019-11-22 12:19:01 +0100

    Statistics completed

--- 03-userInterfacesConfection/01-FinalProject/PresentationLayer/Statistics.Designer.cs
+++ 03-userInterfacesConfection/01-FinalProject/PresentationLayer/Statistics.Designer.cs
@@ -32,17 +32,12 @@
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 10D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint2 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(1D, 25D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint3 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2D, 22D);
             this.titleLbl = new System.Windows.Forms.Label();
             this.monthLbl = new System.Windows.Forms.Label();
             this.monthBox = new System.Windows.Forms.ComboBox();
-            this.typeChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.yearChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.monthChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            ((System.ComponentModel.ISupportInitialize)(this.typeChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yearChart)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.monthChart)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,7 +47,7 @@
             this.titleLbl.AutoSize = true;
             this.titleLbl.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.titleLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(252)))), ((int)(((byte)(250)))));
-            this.titleLbl.Location = new System.Drawing.Point(334, 9);
+            this.titleLbl.Location = new System.Drawing.Point(354, 9);
             this.titleLbl.Name = "titleLbl";
             this.titleLbl.Size = new System.Drawing.Size(296, 70);
             this.titleLbl.TabIndex = 6;
@@ -63,7 +58,7 @@
             this.monthLbl.AutoSize = true;
             this.monthLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.monthLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(252)))), ((int)(((byte)(250)))));
-            this.monthLbl.Location = new System.Drawing.Point(257, 130);
+            this.monthLbl.Location = new System.Drawing.Point(305, 119);
             this.monthLbl.Name = "monthLbl";
             this.monthLbl.Size = new System.Drawing.Size(96, 29);
             this.monthLbl.TabIndex = 58;
@@ -77,34 +72,42 @@
             this.monthBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.monthBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(252)))), ((int)(((byte)(250)))));
             this.monthBox.FormattingEnabled = true;
-            this.monthBox.Location = new System.Drawing.Point(385, 128);
+            this.monthBox.Location = new System.Drawing.Point(437, 116);
             this.monthBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.monthBox.Name = "monthBox";
-            this.monthBox.Size = new System.Drawing.Size(284, 37);
+            this.monthBox.Size = new System.Drawing.Size(302, 37);
             this.monthBox.TabIndex = 59;
+            this.monthBox.SelectedValueChanged += new System.EventHandler(this.FillCharts);
             // 
-            // typeChart
+            // yearChart
             // 
-            this.typeChart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(148)))), ((int)(((byte)(144)))));
-            this.typeChart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.typeChart.BorderlineColor = System.Drawing.Color.Empty;
+            this.yearChart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(148)))), ((int)(((byte)(144)))));
+            this.yearChart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.yearChart.BorderlineColor = System.Drawing.Color.Empty;
             chartArea1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(148)))), ((int)(((byte)(144)))));
             chartArea1.Name = "ChartArea1";
-            this.typeChart.ChartAreas.Add(chartArea1);
+            this.yearChart.ChartAreas.Add(chartArea1);
             legend1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(148)))), ((int)(((byte)(144)))));
             legend1.Name = "Legend1";
-            this.typeChart.Legends.Add(legend1);
-            this.typeChart.Location = new System.Drawing.Point(483, 250);
-            this.typeChart.Margin = new System.Windows.Forms.Padding(4);
-            this.typeChart.Name = "typeChart";
+            this.yearChart.Legends.Add(legend1);
+            this.yearChart.Location = new System.Drawing.Point(505, 208);
+            this.yearChart.Margin = new System.Windows.Forms.Padding(4);
+            this.yearChart.Name = "yearChart";
+            this.yearChart.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.FromArgb(((int)(((byte)(135)))), ((int)(((byte)(61)))), ((int)(((byte)(72))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(119)))), ((int)(((byte)(83))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(203)))), ((int)(((byte)(216))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(149)))), ((int)(((byte)(114))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(86)))), ((int)(((byte)(71)))), ((int)(((byte)(135))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(207)))), ((int)(((byte)(236)))))};
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
-            this.typeChart.Series.Add(series1);
-            this.typeChart.Size = new System.Drawing.Size(487, 320);
-            this.typeChart.TabIndex = 60;
-            this.typeChart.Text = "chart2";
+            this.yearChart.Series.Add(series1);
+            this.yearChart.Size = new System.Drawing.Size(490, 442);
+            this.yearChart.TabIndex = 60;
+            this.yearChart.Text = "chart2";
             // 
             // monthChart
             // 
@@ -112,22 +115,13 @@
             chartArea2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(148)))), ((int)(((byte)(144)))));
             chartArea2.Name = "ChartArea1";
             this.monthChart.ChartAreas.Add(chartArea2);
-            legend2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(148)))), ((int)(((byte)(144)))));
-            legend2.Name = "Legend1";
-            this.monthChart.Legends.Add(legend2);
-            this.monthChart.Location = new System.Drawing.Point(13, 250);
+            this.monthChart.Location = new System.Drawing.Point(5, 208);
             this.monthChart.Margin = new System.Windows.Forms.Padding(4);
             this.monthChart.Name = "monthChart";
-            this.monthChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.MarkerColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            series2.Name = "Series1";
-            series2.Points.Add(dataPoint1);
-            series2.Points.Add(dataPoint2);
-            series2.Points.Add(dataPoint3);
-            this.monthChart.Series.Add(series2);
-            this.monthChart.Size = new System.Drawing.Size(450, 320);
+            this.monthChart.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.monthChart.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(119)))), ((int)(((byte)(83)))))};
+            this.monthChart.Size = new System.Drawing.Size(490, 442);
             this.monthChart.TabIndex = 61;
             this.monthChart.Text = "Grafico 1";
             // 
@@ -136,16 +130,16 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(225)))), ((int)(((byte)(212)))));
-            this.ClientSize = new System.Drawing.Size(982, 643);
+            this.ClientSize = new System.Drawing.Size(1000, 690);
             this.Controls.Add(this.monthChart);
-            this.Controls.Add(this.typeChart);
+            this.Controls.Add(this.yearChart);
             this.Controls.Add(this.monthBox);
             this.Controls.Add(this.monthLbl);
             this.Controls.Add(this.titleLbl);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Statistics";
             this.Text = "Statistics";
-            ((System.ComponentModel.ISupportInitialize)(this.typeChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yearChart)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.monthChart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -157,7 +151,7 @@
         private System.Windows.Forms.Label titleLbl;
         private System.Windows.Forms.Label monthLbl;
         private System.Windows.Forms.ComboBox monthBox;
-        private System.Windows.Forms.DataVisualization.Charting.Chart typeChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart yearChart;
         private System.Windows.Forms.DataVisualization.Charting.Chart monthChart;
     }
 }
