7b9ecf998cdcda75ef18a2c5e339ccd7afb00aec
Adrian Navarro Gabino <adriannavarrogabino@gmail.com>
2020-01-21 18:19:23 +0100

    Search and register users

---
+++ 05-WPF/05-FinalProject/FinalProject/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using BussinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FinalProject
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Business buss;
+
+        public MainWindow(Business buss)
+        {
+            InitializeComponent();
+            this.buss = buss;
+
+            InsertUser insertUser = new InsertUser();
+            insertUser.HorizontalAlignment = HorizontalAlignment.Center;
+            insertUser.VerticalAlignment = VerticalAlignment.Center;
+
+            panel2.Children.Clear();
+            panel2.Children.Add(insertUser);
+        }
+    }
+}
